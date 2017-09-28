19a74f5bfabd3ff03afa0d67b1035e586fa1fb28
abhishekkhedekar <khedekarabhishek@gmail.com>
2017-09-28 21:20:13 +0530

    initial commit

--- KeyChainDemo/ViewController.swift
+++ KeyChainDemo/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import SwiftKeychainWrapper
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtfieldPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func savePassword(_ sender: Any) {
+        
+        if let password = txtfieldPassword.text {
+            let saveSuccesful:Bool = KeychainWrapper.standard.set(password, forKey: "UserPassword")
+            print("save was successful : \(saveSuccesful)")
+            self.view.endEditing(true)
+        }
+        
+    
+        }
+    
+    @IBAction func retrivePassword(_ sender: Any) {
+     
+        if let savedPassword = KeychainWrapper.standard.string(forKey: "UserPassword") {
+            print("password is : \(savedPassword)")
+        }
+    }
+    
+    @IBAction func deleteKeyFromKeychain(_ sender: Any) {
+        let deleteSuccessful:Bool = KeychainWrapper.standard.removeObject(forKey: "UserPassword")
+        print("key deleted : \(deleteSuccessful)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
